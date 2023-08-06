e1d97696520abf2a88083b28f110f6686a44ca7a
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-06 23:27:39 +0600

    middleware

--- src/server.js
+++ src/server.js
@@ -5,12 +5,19 @@ const port = 3001;
 
 
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 
 
 app.get("/", (req, res)=>{
     res.send("Hello")
 })
 
+
+app.use((req, res, next)=>{
+    res.status(404).json({message: "404 not found"})
+})
 
 app.listen(port, ()=>{
     console.log(`Server running on PORT ${port}`)
